4ede8172e25597880e7b657533d3eaf9970b487a
Malonna Allen <malonnaallen@gmail.com>
2021-10-19 18:59:18 -0400

    Updated settings.

--- PunkyCutsProject/PunkyCutsProject/settings.py
+++ PunkyCutsProject/PunkyCutsProject/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'PunkyCutsProject.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'punky_cuts_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
